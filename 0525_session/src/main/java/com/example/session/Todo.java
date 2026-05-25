3d1cdb206a75488236fdc6b41aea5e272af3353b
2sseul <iseul0626@gmail.com>
2026-05-25 13:26:32 +0900

    0525 실습코드

---
+++ 0525_session/src/main/java/com/example/session/Todo.java
@@ -0,0 +1,14 @@
+package com.example.session;
+
+public class Todo {
+
+    // TODO: title 필드 만들기
+
+    // TODO: done 필드 만들기
+
+    // TODO: 기본 생성자 만들기
+
+    // TODO: 전체 필드 생성자 만들기
+
+    // TODO: getter / setter 만들기
+}
---
+++ 0525_session/src/main/java/com/example/session/TodoController.java
@@ -0,0 +1,16 @@
+package com.example.session;
+
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+public class TodoController {
+
+    // TODO: Todo 목록 저장용 List 만들기
+
+    // TODO: GET /todos 구현하기
+
+    // TODO: POST /todos 구현하기
+}
