12a88f3068e19281734de223946caa784cfbca80
seunghyko <shko0919@naver.com>
2026-05-26 22:52:01 +0900

    5회차 과제 제출

--- 0525_session/src/main/java/com/example/session/Todo.java
+++ 0525_session/src/main/java/com/example/session/Todo.java
@@ -3,12 +3,39 @@ package com.example.session;
 public class Todo {
 
     // TODO: private 필드 사용하기
+    private Long id;
     // TODO: title 필드 만들기
+    private String title;
     // TODO: done 필드 만들기
-
+    private boolean done;
     // TODO: 기본 생성자 만들기
+    public  Todo(){
 
+    }
     // TODO: 전체 필드 생성자 만들기
-
+    public Todo(Long id, String title, boolean done){
+        this.id = id;
+        this.title = title;
+        this.done = done;
+    }
     // TODO: getter / setter 만들기
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id = id;
+    }
+    public String getTitle(){
+        return title;
+    }
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+    public boolean isDone(){
+        return done;
+    }
+    public void setDone(boolean done){
+        this.done = done;
+    }
 }
--- 0525_session/src/main/java/com/example/session/TodoController.java
+++ 0525_session/src/main/java/com/example/session/TodoController.java
@@ -9,8 +9,29 @@ import java.util.List;
 public class TodoController {
 
     // TODO: Todo 목록 저장용 List 만들기 ex) List<Todo> ..
+    private final List<Todo> todos = new ArrayList<>();
+    private Long nextId = 1L;
 
     // TODO: GET /todos 구현하기
-
+    @GetMapping("/todos")
+    public List<Todo> getTodos() {
+        return todos;
+    }
     // TODO: POST /todos 구현하기
+    @PostMapping("/todos")
+    public Todo addTodos(@RequestBody Todo todo) {
+        todo.setId(nextId++);
+        todos.add(todo);
+        return todo;
+    }
+
+    @GetMapping("/todos/{id}")
+    public Todo getTodoById(@PathVariable Long id){
+        for (Todo todo : todos){
+            if (todo.getId().equals(id)){
+                return todo;
+            }
+        }
+        return null;
+    }
 }
