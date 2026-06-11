dfdda8769179e6832dd51d387066109d741afd0c
seunghyko <shko0919@naver.com>
2026-06-12 00:27:11 +0900

    feat : JPA 설정

---
+++ seunghyko/jpa-homework/src/main/java/com/likelion/jpahomework/JpaHomeworkApplication.java
@@ -0,0 +1,13 @@
+package com.likelion.jpahomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaHomeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaHomeworkApplication.class, args);
+	}
+
+}
